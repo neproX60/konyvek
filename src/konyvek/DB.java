70dc306216fe45bf3445fc9765c012d80d3ae9e2
neproX60 <>
2018-01-27 10:27:41 +0100

    Alap változat.

---
+++ src/konyvek/DB.java
@@ -0,0 +1,48 @@
+package konyvek;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DB {
+    Connection kapcs = null;
+    Statement parancs = null;
+    ResultSet eredmeny = null;
+    PreparedStatement ekp = null;
+    
+    final String dbUrl = "jdbc:mysql://localhost:3306/konyvek";
+    final String user = "root";
+    final String pass = "";
+    
+    public DB() {
+        try {
+            //kapcsolódás
+            kapcs = DriverManager.getConnection(dbUrl, user, pass);
+            System.out.println("A kapcsolat létrejött.");
+            //parancs létrehozása
+            parancs = kapcs.createStatement();
+        } catch (SQLException ex) {
+            System.out.println("" + ex);
+        }
+    }
+    public void kiir(int ev) {
+        try {
+            ekp = kapcs.prepareStatement("SELECT * FROM adatok WHERE ev = ?");
+            ekp.setInt(1, ev);
+            eredmeny = ekp.executeQuery();
+            while (eredmeny.next()) {
+                System.out.printf("%2d %-30s %-35s %-35s %d\n",
+                                    eredmeny.getInt("id"),
+                                    eredmeny.getString("szerzo"),
+                                    eredmeny.getString("cim"),
+                                    eredmeny.getString("eredeti"),
+                                    eredmeny.getInt("ev"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Hiba a lekérdezésben!");
+        }
+    }
+}
---
+++ src/konyvek/Konyvek.java
@@ -0,0 +1,9 @@
+package konyvek;
+
+public class Konyvek {
+
+    public static void main(String[] args) {
+        DB ab = new DB();
+        ab.kiir(1965);
+    }
+}
