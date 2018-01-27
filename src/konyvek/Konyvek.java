0b53c5caa503dd7fede8dc0311db5018db41916b
neproX60 <t1@192.168.56.1>
2018-01-27 10:46:12 +0100

    Új főprogram.

--- src/konyvek/Konyvek.java
+++ src/konyvek/Konyvek.java
@@ -1,9 +1,38 @@
 package konyvek;
 
+import java.util.Scanner;
+
 public class Konyvek {
 
     public static void main(String[] args) {
         DB ab = new DB();
-        ab.kiir(1965);
+        Scanner bill = new Scanner(System.in, "cp1250");
+        boolean tovabb = true;
+        String szerzo, cim, eredeti;
+        int ev;
+        
+        while (tovabb) {
+            System.out.println("1-listázás 2-új 3-törlés 0-kilépés");
+            System.out.print("Válasz: ");
+            String v = bill.nextLine();
+            switch (v.charAt(0)) {
+                case '1':
+                    System.out.print("Év: ");
+                    ev = bill.nextInt();
+                    bill.nextLine();
+                    ab.kiir(ev);
+                    break;
+                case '2':
+                    
+                    break;
+                case '3':
+                    
+                    break;
+                default:
+                    tovabb = false;
+                    
+                    break;
+            }
+        }
     }
 }
